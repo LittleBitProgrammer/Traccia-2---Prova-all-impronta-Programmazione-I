e07101fbd566a0d8fa6a91242fa3eab607577009
Roberto Vecchio <info@robertovecchio.com>
2019-02-10 17:32:12 +0100

    completato esercizio
    - esercizio completato

---
+++ Prova impronta nr 2/Prova impronta nr 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Prova impronta nr 2
+//
+//  Created by Roberto Vecchio on 10/02/2019.
+//  Copyright © 2019 Roberto Vecchio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
